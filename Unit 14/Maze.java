c52325be72f321600670cf09c79093e0f167c4a4
Philippeh7 <32948434+Philippeh7@users.noreply.github.com>
2018-03-25 22:24:55 -0700

    Unit 14

---
+++ Unit 14/Maze.java
@@ -0,0 +1,83 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Maze
+{
+   private int[][] maze;
+
+	public Maze()
+	{
+
+
+	}
+
+	public Maze(int size, String line)
+	{
+		maze = new int[size][size];
+		Scanner chopper = new Scanner(line);
+	for(int k=0;k<size;k++)
+	{
+		for(int i=0;i<size;i++ )
+		{
+		
+			maze[k][i] = chopper.nextInt();
+			
+		}
+	}
+
+	}
+
+	public boolean hasExitPath(int r, int c)
+	{
+		
+	  if(c == (maze.length-1) && maze[r][c] == 1)
+			return true;
+	  
+	  if(maze[r][c] == 1)
+	  {
+		  maze[r][c] = 0;
+		if(r<(maze.length-1) && maze[r+1][c] == 1 && hasExitPath(r+1,c))
+			    return true;
+		if(r>0 && maze[r-1][c] == 1 && hasExitPath(r-1,c))
+				return true;
+		if(c<(maze.length-1) && maze[r][c+1] == 1 && hasExitPath(r,c+1))
+			return true;
+		if(c>0 && maze[r][c-1] == 1 && hasExitPath(r,c-1))
+				return true;
+	  }
+		
+
+		return false;
+	}
+
+	public String toString()
+	{
+		String output="";
+		for(int k=0;k<maze.length;k++)
+		{
+			for(int i=0;i<maze.length;i++ )
+			{
+			
+				output += maze[k][i];
+				
+			}
+			output += "\n";
+		}
+
+		if(hasExitPath(0,0))
+			output += "\n" + "Has an Exit";
+		else
+			output += "\n" + "Does not have an Exit";
+
+
+		return output;
+	}
+}
